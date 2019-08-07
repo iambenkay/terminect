94a4b6b4d430cd3d14560782345664dff70ae2a9
benkay <benjamin.node@gmail.com>
2019-08-07 09:35:03 +0100

    removed test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-(function(){
-    var P = ["\\Benjamin", "|Kayode", "/Eventuarry", "-Haq Haq Haq"];
-    var x = 0;
-
-    return setInterval(function(){
-        process.stdout.write("\r" + P[x++]);
-        x &= 3;
-    }, 100);
-})();
